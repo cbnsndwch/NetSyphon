fa2c06c6220467f11fed6fcf7932876f750bce63
Sergio Leon <sergelg@pagroupco.com>
2017-06-22 23:54:52 -0400

    configured service registration on app startup

--- NetSyphon/Program.cs
+++ NetSyphon/Program.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using NetSyphon.Cli;
+using NetSyphon.Commands.Contracts;
+using NetSyphon.Commands.Implementations;
+using NetSyphon.IoC;
+using NetSyphon.Models;
 using PowerArgs;
 
 namespace NetSyphon
@@ -10,13 +14,14 @@ namespace NetSyphon
         {
             try
             {
+                // register available commands
+                RegisterCommands();
+
                 args = new[] { "-h" };
                 //args = new[] { "job", "D:\\test.json" };
                 //args = new[] { "new", "D:\\test.json", "D:\\out.json" };
 
                 Args.InvokeAction<Entry>(args);
-
-                Console.ReadLine();
             }
             catch (Exception e)
             {
@@ -24,6 +29,19 @@ namespace NetSyphon
                 Console.WriteLine($"StackTrace: {e.StackTrace}");
             }
 
+#if DEBUG
+            // keep the Console window open if in Debug mode
+            Console.ReadLine();
+#endif
+        }
+
+        /// <summary>
+        /// Register available commands with the Service Registry
+        /// </summary>
+        private static void RegisterCommands()
+        {
+            // register the RunJobCommand implementation
+            ServiceLocator.Instance.RegisterService<ICommand<JobDescription>, RunJobCommand>(nameof(Entry.RunJob));
         }
     }
 }
