94ec30c6627d0dbd656eeccc70d206fcab50aede
Sergio Leon <sergelg@pagroupco.com>
2017-06-22 23:06:13 -0400

    added a service registry skeleton to support IoC via Service Location

---
+++ NetSyphon/IoC/ServiceLocator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetSyphon.IoC
+{
+    public class ServiceLocator
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Private constructor to simulate singleton-like dynamics
+        /// </summary>
+        private ServiceLocator() { }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// A singleton-like point of access to the ServiceLocator
+        /// </summary>
+        public static ServiceLocator Instance = new ServiceLocator();
+
+        #endregion
+
+        #region Public Methods
+
+        public TService Get<TService>(object key = null)
+        {
+            return default(TService);
+        }
+
+        #endregion
+    }
+}
