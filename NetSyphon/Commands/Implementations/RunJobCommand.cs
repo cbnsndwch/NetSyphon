b96d4394c0071bdeb7a54722583e08f10fec1c0c
Sergio Leon <sergelg@pagroupco.com>
2017-06-22 22:47:10 -0400

    added a RunJob command implementation and its model

---
+++ NetSyphon/Commands/Implementations/RunJobCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NetSyphon.Commands.Contracts;
+using NetSyphon.Models;
+
+namespace NetSyphon.Commands.Implementations
+{
+    /// <summary>
+    /// A command to run and ETL job, as specified by the Job Configuration model
+    /// </summary>
+    public class RunJobCommand : ICommand<RunJobModel>
+    {
+        /// <summary>
+        /// The entry point to the command
+        /// </summary>
+        /// <param name="model">An instance of <see cref="RunJobModel"/> describing the ETL job</param>
+        public void Execute(RunJobModel model)
+        {
+            if (model==null)
+                throw new ArgumentException("The Job Description model cannot be null");
+        }
+    }
+}
