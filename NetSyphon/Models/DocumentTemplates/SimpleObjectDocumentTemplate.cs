59d1d4aab92b745445fcf02c3dd420c87202537b
Sergio Leon <sergelg@pagroupco.com>
2017-06-24 17:13:23 -0400

    added support for nested objects

--- NetSyphon/Models/DocumentTemplates/SimpleObjectDocumentTemplate.cs
+++ NetSyphon/Models/DocumentTemplates/SimpleObjectDocumentTemplate.cs
@@ -39,25 +39,19 @@ namespace NetSyphon.Models.DocumentTemplates
                 else if (kv.Value is string)
                 {
                     var strVal = (string)kv.Value;
-                    if (strVal.StartsWith("$"))
-                    {
-                        // a column
-
-
-                    }
-                    else if (strVal.StartsWith("@"))
+                    if (strVal.StartsWith("@"))
                     {
                         // a Lookup Table
                     }
                     else
                     {
-                        // a literal string property
-                        val = GetScalarValue(src, kv);
+                        // a literal string property or a column value
+                        val = GetScalarValue(new KeyValuePair<string, object>(kv.Key, strVal), src);
                     }
                 }
                 else
                 {
-                    val = GetScalarValue(src, kv);
+                    val = GetScalarValue(kv, src);
                 }
 
                 // only add the value if it is not NULL
@@ -69,25 +63,35 @@ namespace NetSyphon.Models.DocumentTemplates
             return result;
         }
 
-        private object GetScalarValue(IDictionary<string, object> src, KeyValuePair<string, object> kv)
+        private object GetScalarValue(KeyValuePair<string, object> kv, IDictionary<string, object> src)
         {
-            if (!src.ContainsKey(kv.Key))
+            if (kv.Value == null)
                 return null;
 
-            var v = src[kv.Key];
-            if (v == null)
-                return null;
+            //var v = src[kv.Key];
+            //if (v == null)
+            //    return null;
 
-            switch (v.GetType())
+            //if (!src.ContainsKey(kv.Key))
+            //    return null;
+
+            switch (kv.Value.GetType())
             {
                 case Type t when t == typeof(string):
-                    var val = ((string)kv.Value).StartsWith("$") ? src[kv.Key.TrimStart('$')] : kv.Value;
+
+                    var strVal = (string) kv.Value;
+                    var val = strVal.StartsWith("$") 
+                        ? src[strVal.TrimStart('$')] 
+                        : kv.Value;
 
                     // as of Driver version 2.4, System.Decimal gets serialized to MongoDB as a nested object
                     // HACK: cast it to double
                     return val is decimal ? Convert.ToDouble(val) : val;
+                case Type t when t == typeof(JToken):
+                    var jt = kv.Value;
+                    return null;
                 case Type t when t == typeof(JObject):
-                    var xxx = kv.Value;
+                    var jo = kv.Value;
                     return null;
                 case Type decimalType when decimalType == typeof(decimal):
                     // as of Driver version 2.4, System.Decimal gets serialized to MongoDB as a nested object
@@ -106,13 +110,67 @@ namespace NetSyphon.Models.DocumentTemplates
             }
         }
 
-        private object GetObjectValue(IDictionary<string, object> src, JObject kv)
+        private object GetObjectValue(IDictionary<string, object> src, JObject tpl)
         {
             var result = new ExpandoObject();
             var dst = (IDictionary<string, object>)result;
 
+            foreach (var kv in tpl)
+            {
+                if (kv.Value == null)
+                    continue;
+
+                object val = null;
+                if (kv.Value.GetType() == typeof(JArray))
+                {
+                    var arr = ((JArray)kv.Value).ToArray();
+                    val = null;
+                }
+                else if (kv.Value.GetType() == typeof(JObject))
+                {
+                    // an object property
+                    val = GetObjectValue(src, (JObject)kv.Value);
+                }
+                else if (kv.Value is JValue)
+                {
+                    // cast to work with JValue members
+                    var jval = (JValue)kv.Value;
+
+                    switch (jval.Type)
+                    {
+                        case JTokenType.String:
+                            var strVal = (string)kv.Value;
+                            if (strVal.StartsWith("@"))
+                            {
+                                // a Lookup Table
+                            }
+                            else
+                            {
+                                // a literal string property or a column value
+                                val = GetScalarValue(new KeyValuePair<string, object>(kv.Key, strVal), src);
+                            }
+                            break;
+                        case JTokenType.Boolean:
+                        case JTokenType.Bytes:
+                        case JTokenType.Date:
+                        case JTokenType.Float:
+                        case JTokenType.Guid:
+                        case JTokenType.Integer:
+                        case JTokenType.TimeSpan:
+                            val = GetScalarValue(new KeyValuePair<string, object>(kv.Key, kv.Value), src);
+                            break;
+
+                    }
 
 
+                }
+
+
+                // only add the value if it is not NULL
+                // TODO: Allow overriding this via a Section/Job setting
+                if (val != null)
+                    dst[kv.Key] = val;
+            }
 
             return result;
         }
